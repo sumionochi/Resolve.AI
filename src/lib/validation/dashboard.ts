36eceff948d4a3b1dcdf4c0acf50e5b61b29c897
sumionochi <aaditya.srivastava.connect@gmail.com>
2024-01-14 18:26:15 +0530

    validation is ready for resolution form

---
+++ src/lib/validation/dashboard.ts
@@ -0,0 +1,24 @@
+import { z } from "zod";
+
+export const createAssessSchema = z.object({
+  goal: z.string().min(1, { message: "Goal is required" }),
+  theme: z.string().min(1, { message: "Theme is required" }),
+  timeframeFrom: z.string().min(1, { message: "Initial Date is required" }),
+  timeframeTo: z
+    .string()
+    .min(1, { message: "Expected Date to reach your goal is required" }),
+  describe: z.string().min(1, { message: "Elaborate your Goal is required" }),
+  nextSteps: z.array(z.string()).refine((data) => data.length >= 3, {
+    message: "At least three steps are required",
+  }),
+});
+
+export const updateAssessSchema = createAssessSchema.extend({
+  id: z.string().min(1),
+});
+
+export const deleteAssessSchema = z.object({
+  id: z.string().min(1),
+});
+
+export type CreateAssessSchema = z.infer<typeof createAssessSchema>;
