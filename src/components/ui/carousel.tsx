7b0fe99804350cd2c81c618fd63bcafa19a0393f
sumionochi <aaditya.srivastava.connect@gmail.com>
2024-01-14 18:05:39 +0530

    fixed some import problems

--- src/components/ui/carousel.tsx
+++ src/components/ui/carousel.tsx
@@ -1,11 +1,12 @@
 "use client"
 
 import * as React from "react"
-import useEmblaCarousel, {
+import useEmblaCarousel from 'embla-carousel-react';
+import {
   type EmblaCarouselType as CarouselApi,
   type EmblaOptionsType as CarouselOptions,
   type EmblaPluginType as CarouselPlugin,
-} from "embla-carousel-react"
+} from 'embla-carousel';
 import { ArrowLeft, ArrowRight } from "lucide-react"
 
 import { cn } from "@/lib/utils"
@@ -203,7 +204,7 @@ const CarouselPrevious = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute  h-8 w-8 hidden rounded-full",
+        "absolute  h-8 w-8 rounded-full",
         orientation === "horizontal"
           ? "-left-12 top-1/2 -translate-y-1/2"
           : "-top-12 left-1/2 -translate-x-1/2 rotate-90",
@@ -232,7 +233,7 @@ const CarouselNext = React.forwardRef<
       variant={variant}
       size={size}
       className={cn(
-        "absolute h-8 w-8 hidden rounded-full",
+        "absolute h-8 w-8 rounded-full",
         orientation === "horizontal"
           ? "-right-12 top-1/2 -translate-y-1/2"
           : "-bottom-12 left-1/2 -translate-x-1/2 rotate-90",
